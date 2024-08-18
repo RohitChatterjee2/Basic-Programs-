2941de8c07e92bf27b4daec5b95368be2354243f
Rohit Chatterjee <rohitchatterjee758@gmail.com>
2024-08-18 12:40:10 +0530

    Add files via upload

---
+++ stringinput.py
@@ -0,0 +1,11 @@
+# see what happens when numbers are taken in string 
+num1=input("Enter the no. :")
+num2=input("Enter the second number .:")
+print(num1+num2)
+
+
+
+"""  OUTPUT :
+     Enter the no. :5
+     Enter the second number .:2
+     52   """
